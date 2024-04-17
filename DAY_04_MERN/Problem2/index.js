21eb60d138a635166b18c910997980cdcb533e5a
parteek kumar <70316086+PART00007@users.noreply.github.com>
2024-04-17 23:40:29 +0530

    Update index.js

--- DAY_04_MERN/Problem2/index.js
+++ DAY_04_MERN/Problem2/index.js
@@ -1,1 +1,36 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// Using native map function
+const doubledNumbersNative = numbers.map(num => num * 2);
+console.log('Doubled Numbers (Using Native map):', doubledNumbersNative);
+
+// Using custom map function
+function customMap(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i], i, array));
+    }
+    return newArray;
+}
+
+const doubledNumbersCustom = customMap(numbers, num => num * 2);
+console.log('Doubled Numbers (Using Custom map):', doubledNumbersCustom);
+
+const numbers = [1, 2, 3, 4, 5];
+
+// Using native map function
+const doubledNumbersNative = numbers.map(num => num * 2);
+console.log('Doubled Numbers (Using Native map):', doubledNumbersNative);
+
+// Using custom map function
+function customMap(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i], i, array));
+    }
+    return newArray;
+}
+
+const doubledNumbersCustom = customMap(numbers, num => num * 2);
+console.log('Doubled Numbers (Using Custom map):', doubledNumbersCustom);
 
